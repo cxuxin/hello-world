3fe703ff6e7737bba170e3266fa71c8ed3fe1d7f
cxuxin <34834221+cxuxin@users.noreply.github.com>
2019-09-18 11:22:39 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : CXX
+# @Time    : 2019/9/17
+# @File    : test.py
+# @Software: PyCharm
+# @Project : Test
+# @Desc    :
+
+print("hello")
